3a4af9880b999168523134e1d61dd003dd30a3a1
Jonilson Sousa <jonilsondeveloper@gmail.com>
2017-04-12 15:48:40 -0300

    Site com menu top-down
    menu certo e estilizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-  $('.right.menu.open').on("click",function(e){
-    e.preventDefault();
-    $('.ui.vertical.menu').toggle();
-  });
+// $(document).ready(function(){
+//   $('.right.menu.open').on("click",function(e){
+//     e.preventDefault();
+//     $('.ui.vertical.menu').toggle();
+//   });
+//
+//   $('.ui.dropdown').dropdown();
+// });
 
-  $('.ui.dropdown').dropdown();
+$(document).ready(function(){
+  $('.handle').click(function() {
+    $('#nav ul').toggleClass('showing');
+  }),
+
+  $('#faleconosco').click(function () {
+    $("#content").load("faleconosco.html");
+  });
 });
